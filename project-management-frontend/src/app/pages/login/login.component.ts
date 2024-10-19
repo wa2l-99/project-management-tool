b398834711281e291a2d2324ad7e4d204507f345
Wael Zantour <waelzantour1@gmail.com>
2024-10-19 23:01:07 +0200

    fix the main and the loogin components

--- project-management-frontend/src/app/pages/login/login.component.ts
+++ project-management-frontend/src/app/pages/login/login.component.ts
@@ -28,7 +28,7 @@ export class LoginComponent {
     this.authService.authenticate({ body: this.authrequest }).subscribe({
       next: (res) => {
         this.tokenService.token = res.token as string;
-        this.router.navigate(['projects']);
+        this.router.navigate(['/']);
         this.toastr.success('Connexion réussie !', 'Succès');
 
         if (res.user) {
