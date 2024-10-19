4c35f3e576e46776c0bbf39595ee310dee2e2ec5
Wael Zantour <waelzantour1@gmail.com>
2024-10-19 23:00:29 +0200

    create sidebar service and fix the sidebar component

--- project-management-frontend/src/app/modules/projects/components/sidebar/sidebar.component.ts
+++ project-management-frontend/src/app/modules/projects/components/sidebar/sidebar.component.ts
@@ -1,28 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarService } from '../../../../services/sidebarService/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   isToggled: boolean = false;
   isCollapsed: boolean = false;
 
-  constructor() {}
+  constructor(private sidebarService: SidebarService) {}
 
+  ngOnInit(): void {
+    const linkColor = document.querySelectorAll('.nav-link');
+    linkColor.forEach((link) => {
+      if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+        link.classList.add('active');
+      }
+      link.addEventListener('click', () => {
+        linkColor.forEach((l) => l.classList.remove('active'));
+        link.classList.add('active');
+      });
+    });
+
+    this.sidebarService.isSidebarOpen$.subscribe((isOpen) => {
+      this.isToggled = isOpen;
+
+      // Fermer le collapse si la sidebar est fermée
+      if (!this.isToggled && this.isCollapsed) {
+        this.isCollapsed = false;
+      }
+    });
+  }
+
+  toggleSidebar() {
+    this.sidebarService.toggleSidebar();
+  }
   // Fonction pour basculer le collapse
   toggleCollapse(event: Event) {
     event.preventDefault();
     this.isCollapsed = !this.isCollapsed;
-  }
-
-  toggleSidebar() {
-    this.isToggled = !this.isToggled;
-
-    // Si la sidebar est fermée et qu'un collapse est ouvert, ferme le collapse
-    if (this.isToggled && this.isCollapsed) {
-      this.isCollapsed = false;
-    }
   }
 }
