6856df7acbc130f0bcb6776611603d350ec2ff07
Wael Zantour <waelzantour1@gmail.com>
2024-11-02 15:47:54 +0100

    add create button to the sideBar

--- project-management-frontend/src/app/modules/projects/components/sidebar/sidebar.component.ts
+++ project-management-frontend/src/app/modules/projects/components/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService } from '../../../../services/sidebarService/sidebar.service';
+import { StorageUserService } from '../../../../services/storageUser/storage-user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,11 +11,17 @@ import { SidebarService } from '../../../../services/sidebarService/sidebar.serv
 export class SidebarComponent implements OnInit {
   isToggled: boolean = false;
   isCollapsed: boolean = false;
+  isActive: boolean = false;
 
-  constructor(private sidebarService: SidebarService) {}
+  constructor(
+    private sidebarService: SidebarService,
+    private storageUserService: StorageUserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const linkColor = document.querySelectorAll('.nav-link');
+
     linkColor.forEach((link) => {
       if (window.location.href.endsWith(link.getAttribute('href') || '')) {
         link.classList.add('active');
@@ -37,9 +45,34 @@ export class SidebarComponent implements OnInit {
   toggleSidebar() {
     this.sidebarService.toggleSidebar();
   }
+
   // Fonction pour basculer le collapse
   toggleCollapse(event: Event) {
     event.preventDefault();
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  isAdmin(): boolean {
+    return this.storageUserService.hasRole('ADMIN');
+  }
+
+  isMember(): boolean {
+    return this.storageUserService.hasRole('MEMBER');
+  }
+
+  isobserver(): boolean {
+    return this.storageUserService.hasRole('OBSERVER');
+  }
+
+  noRole(): boolean {
+    return this.storageUserService.noRole();
+  }
+
+  createProject() {
+    this.router.navigate(['new-projet']).then(() => {
+      document.querySelectorAll('.nav-link').forEach((link) => {
+        link.classList.remove('active');
+      });
+    });
   }
 }
